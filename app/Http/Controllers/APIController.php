958e0cef08be6688c2191ec1d9f0f36f0427177b
unknown <kemalcrisannnaufal@gmail.com>
2024-05-22 20:49:52 +0700

    add Score API n pagination

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -10,10 +10,17 @@ class APIController extends Controller
     public function getData()
     {
         $client = new Client();
-        $response = $client->request('GET', 'https://livescore-api.com/api-client/fixtures/matches.json?key=e6l0EOXMmvzA4Ckl&secret=SGWyI0VUktY9AeLPl6QZVOMijmSnYcJR&competition_id=244');
+        $response = $client->request('GET', 'https://livescore-api.com/api-client/scores/history.json?key=e6l0EOXMmvzA4Ckl&secret=SGWyI0VUktY9AeLPl6QZVOMijmSnYcJR&competition_id=244&from=2024-01-01');
         $data = json_decode($response->getBody(), true);
-        dd($data);
-        return view('test', ['data' => $data['data']['table']]);
+
+
+        $latest_match = array_slice($data, -1);
+        $matches = array_slice($data, 0, count($data) - 1);
+
+        return view('test', [
+            'latest_match' => $latest_match,
+            'matches' => $matches
+        ]);
 
     }
 }
--- resources/views/score/score.blade.php
+++ resources/views/score/score.blade.php
@@ -5,171 +5,90 @@
 @section('css', '/css/score/score-style.css')
 
 @section('content')
-    <div class="container mt-5">
-        <div class="row d-flex justify-content-center">
-            <div class="col-md-10">
-                <div class="img-box">
-                    <div id="img">
-                        <img src="images/score.png" alt="" class="img">
+<div class="container mt-5">
+    <div class="row d-flex justify-content-center">
+        <div class="col-md-10">
+            <div class="img-box">
+                <div id="img">
+                    <img src="images/score.png" alt="" class="img">
+                </div>
+                <div class="d-flex justify-content-center align-items-center gap-3 text-white" id="main-score">
+                    <div class="d-flex flex-column align-items-center justify-content-center">
+                        <p class="text-center fw-bold fs-2">{{ $latest_match[0]['home_name'] }}</p>
                     </div>
-                    <div class="d-flex justify-content-center align-items-center gap-5 text-white" id="main-score">
-                        <div class="d-flex flex-column align-items-center justify-content-center">
-                            <img src="/images/madrid.png" alt="" width="70">
-                            <p class="text-center fw-bold fs-4">Real Madrid</p>
-                        </div>
-                        <div class="text-center fw-bold fs-3">
-                            <p>2 - 1</p>
-                            <P>FULL TIME</P>
-                        </div>
-                        <div class="d-flex flex-column align-items-center justify-content-center">
-                            <img src="/images/barca.png" alt="" width="70">
-                            <p class="text-center fw-bold fs-4">Barcelona FC</p>
-                        </div>
+                    <div class="text-center fw-bold fs-3">
+                        <p>{{ $latest_match[0]['score'] }}</p>
+                        <P class="text-white fw-bold fs-4">FULL TIME</P>
                     </div>
-                    <div class="date-main-info">
-                        <p>UEFA Champions League</p>
-                        <p>SEPT 30 7.45 PM</p>
-                    </div>
-                    <div class="additional-main-info">
-                        <div class="d-flex gap-3 align-items-center">
-                            <i class="fas fa-trophy"></i>
-                            <p>Semifinal</p>
-                        </div>
-                        <div class="d-flex gap-3 align-items-center">
-                            <i class="far fa-clock  "></i>
-                            <p>7.45 PM</p>
-                        </div>
-                        <div class="d-flex gap-3 align-items-center">
-                            <i class="fas fa-map-marker-alt"></i>
-                            <p>Camp Nou Stadium</p>
-                        </div>
+                    <div class="d-flex flex-column align-items-center justify-content-center">
+                        <p class="text-center fw-bold fs-2">{{ $latest_match[0]['away_name'] }}</p>
                     </div>
                 </div>
-
-                <div class="container mt-3">
-                    <p class="fw-bold fs-3 mb-2 text-dark">Result of the other matches</p>
-                    <div class="row">
-                        <div class="col-md-6">
-                            @for ($i = 0; $i < 2; $i++)
-                                <div class="score-main-box">
-                                    <div class="score-box">
-                                        <div class="d-flex flex-column justify-content-center align-items-center">
-                                            <img src="/images/barca.png" alt="" width="40">
-                                            <p class="fw-bold fs-5">Barcelona</p>
-                                        </div>
-                                        <div>
-                                            <p class="fw-bold fs-2">2-1</p>
-                                        </div>
-                                        <div class="d-flex flex-column justify-content-center align-items-center">
-                                            <img src="/images/madrid.png" alt="" width="40">
-                                            <p class="fw-bold fs-5">Real Madrid</p>
-                                        </div>
-                                    </div>
-                                    <div class="d-flex justify-content-between mt-2 p-2 gap-2">
-                                        <div class="d-flex gap-3 align-items-center">
-                                            <i class="fas fa-trophy"></i>
-                                            <p>Semifinal</p>
-                                        </div>
-                                        <div class="d-flex gap-3 align-items-center">
-                                            <i class="far fa-clock  "></i>
-                                            <p>7.45 PM</p>
-                                        </div>
-                                        <div class="d-flex gap-3 align-items-center">
-                                            <i class="fas fa-map-marker-alt"></i>
-                                            <p>Camp Nou Stadium</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            @endfor
-                        </div>
-                        <div class="col-md-6">
-                            @for ($i = 0; $i < 2; $i++)
-                                <div class="score-main-box">
-                                    <div class="score-box">
-                                        <div class="d-flex flex-column justify-content-center align-items-center">
-                                            <img src="/images/barca.png" alt="" width="40">
-                                            <p class="fw-bold fs-5">Barcelona</p>
-                                        </div>
-                                        <div>
-                                            <p class="fw-bold fs-2">2-1</p>
-                                        </div>
-                                        <div class="d-flex flex-column justify-content-center align-items-center">
-                                            <img src="/images/madrid.png" alt="" width="40">
-                                            <p class="fw-bold fs-5">Real Madrid</p>
-                                        </div>
-                                    </div>
-                                    <div class="d-flex justify-content-between mt-2 p-2 gap-2">
-                                        <div class="d-flex gap-3 align-items-center">
-                                            <i class="fas fa-trophy"></i>
-                                            <p>Semifinal</p>
-                                        </div>
-                                        <div class="d-flex gap-3 align-items-center">
-                                            <i class="far fa-clock  "></i>
-                                            <p>7.45 PM</p>
-                                        </div>
-                                        <div class="d-flex gap-3 align-items-center">
-                                            <i class="fas fa-map-marker-alt"></i>
-                                            <p>Camp Nou Stadium</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            @endfor
-                        </div>
-
+                <div class="date-main-info">
+                    <p>UEFA Champions League</p>
+                    @php
+                        $utcTime = $latest_match[0]['date'] . ' ' . $latest_match[0]['scheduled'];
+                        $localTime = Carbon\Carbon::parse($utcTime)
+                            ->setTimezone('Asia/Jakarta')
+                            ->format('Y-m-d H:i');
+                        $time = explode(' ', $localTime);
+                    @endphp
+                    <p>{{ $localTime }}</p>
+                </div>
+                <div class="additional-main-info">
+                    <div class="d-flex gap-3 align-items-center">
+                        <i class="far fa-clock"></i>
+                        <p>{{ $time[1] . ' WIB' }}</p>
+                    </div>
+                    <div class="d-flex gap-3 align-items-center">
+                        <i class="fas fa-map-marker-alt"></i>
+                        <p>{{ $latest_match[0]['location'] }}</p>
                     </div>
                 </div>
             </div>
-        </div>
 
+            <div class="container mt-3">
+                <p class="fw-bold fs-3 mb-2 text-dark">Result of the other matches</p>
+                <div class="row">
+                    @foreach ($matches as $match)
+                        <div class="col-md-6 mb-4">
+                            <div class="score-main-box">
+                                <div class="score-box">
+                                    <div class="d-flex flex-column justify-content-center align-items-center">
+                                        <p class="fw-bold fs-5">{{ $match['home_name'] }}</p>
+                                    </div>
+                                    <div>
+                                        <p class="fw-bold fs-2">{{ $match['score'] }}</p>
+                                    </div>
+                                    <div class="d-flex flex-column justify-content-center align-items-center">
+                                        <p class="fw-bold fs-5">{{ $match['away_name'] }}</p>
+                                    </div>
+                                </div>
+                                <div class="d-flex justify-content-between mt-2 p-2 gap-2">
+                                    <div class="d-flex gap-3 align-items-center">
+                                        <i class="far fa-clock"></i>
+                                        @php
+                                            $utcTime = $match['date'] . ' ' . $match['scheduled'];
+                                            $localTime = Carbon\Carbon::parse($utcTime)
+                                                ->setTimezone('Asia/Jakarta')
+                                                ->format('Y-m-d H:i');
+                                        @endphp
+                                        <p>{{ $localTime }}</p>
+                                    </div>
+                                    <div class="d-flex gap-3 align-items-center">
+                                        <i class="fas fa-map-marker-alt"></i>
+                                        <p>{{ $match['location'] }}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+                <div class="d-flex justify-content-center">
+                    {{ $matches->links('pagination::bootstrap-4') }}
+                </div>
+            </div>
+        </div>
     </div>
-    {{-- <div class="container mt-5">
-        <div class="row d-flex justify-content-center">
-            <div class="col-md-10 d-flex justify-content-center">
-                <div class="score-main-box">
-                    <div class="img-box">
-                        <img src="images/score.png" alt="" class="img">
-                    </div>
-                    <div class="d-flex justify-content-center align-items-center gap-5 text-white" id="main-score">
-                        <div class="d-flex flex-column align-items-center justify-content-center">
-                            <img src="/images/madrid.png" alt="" width="70">
-                            <p class="text-center fw-bold fs-4">Real Madrid</p>
-                        </div>
-                        <div class="text-center fw-bold fs-3">
-                            <p>2 - 1</p>
-                            <P>FULL TIME</P>
-                        </div>
-                        <div class="d-flex flex-column align-items-center justify-content-center">
-                            <img src="/images/barca.png" alt="" width="70">
-                            <p class="text-center fw-bold fs-4">Barcelona FC</p>
-                        </div>
-                    </div>
-                    <div class="date-main-info">
-                        <p>UEFA Champions League</p>
-                        <p>SEPT 30 7.45 PM</p>
-                    </div>
-                    <div class="additional-main-info">
-                        <div class="d-flex gap-3 align-items-center">
-                            <i class="fas fa-trophy"></i>
-                            <p>Semifinal</p>
-                        </div>
-                        <div class="d-flex gap-3 align-items-center">
-                            <i class="far fa-clock  "></i>
-                            <p>7.45 PM</p>
-                        </div>
-                        <div class="d-flex gap-3 align-items-center">
-                            <i class="fas fa-map-marker-alt"></i>
-                            <p>Camp Nou Stadium</p>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-            <div class="row">
-                <div class="col-md-6">
-
-                </div>
-                <div class="col-md-6 bg-success"></div>
-            </div>
-        </div>
-    </div> --}}
+</div>
 @endsection
